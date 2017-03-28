aa1fea9cfe7ca7bca5b1228b33f4cefe7bd31396
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2017-03-28 11:47:28 +0800

    refactor

--- CLRS/Linked List/linked_list.c
+++ CLRS/Linked List/linked_list.c
@@ -15,9 +15,10 @@ void set_values_to_nodes(struct node *head, int data, struct node *n){
 // print the complete linked list
 void print_list(struct node *n){
 	while(n){
-		printf(" %d\n", n->data);
+		printf(" %d ", n->data);
 		n = n->next;
 	}
+	printf("\n");
 }
 
 // push a node into this list at the beginning
@@ -159,6 +160,140 @@ int get_Nth(struct node* head, int n){
     return temp->data;
 }
 
+//reverses a linked list recursively
+void reverse_list(struct node** head_ref){
+    if(!head_ref){
+        return;
+    }
+    reverse_util(*head_ref, NULL, head_ref);
+}
+
+void reverse_util(struct node *curr, struct node* prev, struct node** head_ref){
+    if(!curr->next){
+        *head_ref = curr;
+        curr->next = prev;
+        return;
+    }
+
+    struct node* next = curr->next;
+    curr->next = prev;
+    reverse_util(next, curr, head_ref);
+}
+
+// reverses a list iteratively
+void reverse_list_2(struct node** head_ref){
+    struct node* prev = NULL;
+    struct node* current = *head_ref;
+    struct node* next;
+
+    while(current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head_ref = prev;
+}
+
+// compares if lists are equal
+int compare_lists(struct node* head1, struct node* head2){
+    struct node* temp1 = head1;
+    struct node* temp2 = head2;
+
+    while(temp1 && temp2){
+        if(temp1->data == temp2->data){
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }else{
+            return 0;
+        }
+    }
+
+    if(temp1==NULL && temp2==NULL){
+        return 1;
+    }
+    return 0;
+}
+
+// checks if a list is a palindrome
+int is_palindrome(struct node* head){
+    struct node* slow_ptr = head;
+    struct node* fast_ptr = head;
+    struct node* second_half;
+    struct node* prev_of_slow_ptr = head;
+    struct node* mid_node = NULL;
+    int res = 1;
+
+    if(head!=NULL && head->next!=NULL){
+        while(fast_ptr != NULL && fast_ptr->next != NULL){
+            fast_ptr = fast_ptr->next->next;
+            prev_of_slow_ptr = slow_ptr;
+            slow_ptr = slow_ptr->next;
+        }
+        //now the slow_ptr is on the middle of the list
+
+        //if the length is odd
+        if(fast_ptr!=NULL){
+            mid_node = slow_ptr;
+            slow_ptr = slow_ptr->next;
+        }
+
+        second_half = slow_ptr;
+        prev_of_slow_ptr->next = NULL;
+        reverse_list(&second_half);
+        res = compare_lists(head, second_half);
+
+        reverse_list(&second_half);
+
+        if(mid_node){
+            prev_of_slow_ptr->next = mid_node;
+            mid_node->next = second_half;
+        }else{
+            prev_of_slow_ptr->next = second_half;
+        }
+    }
+
+    return res;
+}
+
+// get intersection util function
+int _get_intersection_node(int d, struct node* head1, struct node* head2){
+    int i;
+    struct node* current1 = head1;
+    struct node* current2 = head2;
+
+    for(i=0;i<d;i++){
+        if(current1==NULL){
+            return -1;
+        }
+        current1 = current1->next;
+    }
+    while(current1 != NULL && current2 != NULL){
+        if(current1 == current2){
+            return current1->data;
+        }
+        current1 = current1->next;
+        current2 = current2->next;
+    }
+    return -1;
+}
+
+// get intersection function
+int get_intersection_node(struct node* head1, struct node* head2){
+    int c1 = get_length_iteratively(head1);
+    int c2 = get_length_iteratively(head2);
+    int d;
+
+    if(c1 > c2){
+        d = c1 - c2;
+        return _get_intersection_node(d, head1, head2);
+    }else{
+        d = c2 - c1;
+        return _get_intersection_node(d, head2, head1);
+    }
+}
+
 int main(){
     struct node *head = NULL;
     struct node *second = NULL;
@@ -176,20 +311,18 @@ int main(){
 
 //    print_list(head);
 
-    insert_after(head, 4);
+    insert_after(head, 3);
 
-    append(&head, 5);
-
-    print_list(head);
-
-    delete_node_at_position(&head, 2);
+    append(&head, 1);
 
     print_list(head);
 
-    printf("\n");
+    reverse_list_2(&head);
 
-    printf("%d\n", get_length_iteratively(head));
-    printf("%d\n", search_key_iteratively(head, 2));
+    print_list(head);
+
+    printf("%d\n", is_palindrome(head));
+
 
     return 0;
 }
